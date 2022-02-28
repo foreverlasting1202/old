96bab9fd3fd0ff34ed74ede607ef780215e907f5
foreverlasting1202 <573902690@qq.com>
2022-02-28 22:09:00 +0800

    Site updated: 2022-02-28 22:07:27

---
+++ js/excerpt.js
@@ -0,0 +1,17 @@
+var rExcerpt = /<!-- more -->/; 
+
+hexo.extend.filter.register('after_post_render', function(data) { 
+    var content = data.content; 
+
+    if (rExcerpt.test(content)){ 
+     data.content = content.replace(rExcerpt, function(match, index){ 
+      data.excerpt = content.substring(0, index).trim(); 
+      data.more = content.substring(index + match.length).trim(); 
+
+      return '<a id="more"></a>'; 
+     }); 
+    } else { 
+     data.excerpt = ''; 
+     data.more = content; 
+    } 
+}); 
